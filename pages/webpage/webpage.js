7b13230e6f17ce24bf71188ac62ccee34e73cb13
Leslie-Fang <fangjiahao@sjtu.edu.cn>
2016-10-26 11:45:32 +0800

    a

--- pages/webpage/webpage.js
+++ pages/webpage/webpage.js
@@ -9,19 +9,20 @@ onLoad: function () {
   onShow:function(){
     console.log('onShow,webpage')
   },
+  //fail for not ready in the server side
   connect:function(){
     this.setData({myname:"leslie"})
-
     wx.chooseImage({
     success: function(res) {
         var tempFilePaths = res.tempFilePaths
         wx.uploadFile({
-        url: 'http://10.189.76.110:8082/MyWeb/rest/UserService/uploadimage',
+       // url: 'http://10.189.76.110:8082/MyWeb/rest/UserService/uploadimage',
+       url: 'http://192.168.1.102:8082/MyWeb/rest/UserService/uploadimage',
         filePath: tempFilePaths[0],
         name: 'firstfile',
-        formData:{
-            'user': 'test'
-        }
+        //formData:{
+        //    'user': 'test'
+        //}
         })
     }
     })
